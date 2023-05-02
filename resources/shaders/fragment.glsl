faa8b616e5da51ffb29eef2cd8e80e1f23729178
red-robby <jakobsorensen@berkeley.edu>
2023-05-01 18:51:17 -0700

    Clean up fragment shader, switch to camel case

--- resources/shaders/fragment.glsl
+++ resources/shaders/fragment.glsl
@@ -17,30 +17,39 @@ const float M = 2.0e30;
 const float rho = 4.0e10;
 const float a = 10.0; //rho / 10.0;
 
+struct ConstantsOfMotion {
+    float b;
+    float B2;
+};
+
+
 struct Ray {
-  /* Camera position. */
+  /* Position. */
   float l; 
   float theta;
   float phi;
 
-  /* Light ray's canonical momenta. */
+  /* Canonical momenta. */
   float p_l;
   float p_theta;
   float p_phi;
+
+  ConstantsOfMotion constants;
 };
 
-vec3 global_spherical_polar_basis(vec2 dir) { // this should be theta and phi
+vec3 globalSphericalPolarBasis(vec2 dir) { // this should be theta and phi
   return vec3(sin(dir.x) * cos(dir.y),
 	      sin(dir.x) * sin(dir.y),
 	      cos(dir.x));
 }
 
 /* ----- Constants of motion. ----- */
-float constants_of_motion_b(Ray ray) {
+
+float constantsOfMotion_b(Ray ray) {
   return ray.p_phi;
 }
 
-float constants_of_motion_B2(Ray ray) {
+float constantsOfMotion_B2(Ray ray) {
   float sin_theta = sin(ray.theta);
   return ray.p_theta * ray.p_theta + (ray.p_phi * ray.p_phi) / (sin_theta * sin_theta);
 }
@@ -49,7 +58,7 @@ float constants_drdl(Ray ray) {
   return (2.0 / PI) * atan(2.0 * ray.l / (PI * M));  
 }  
 
-float constants_radius(Ray ray){
+float radius(Ray ray) {
   float abs_l = abs(ray.l);
   if (abs_l > a) {
     float x = 2.0 * (abs(ray.l) - a) / (PI * M);
@@ -66,69 +75,29 @@ float delta_l(Ray ray) {
 }
 
 float delta_theta(Ray ray) {
-  return ray.p_theta / (constants_radius(ray) * constants_radius(ray));
+  float r = radius(ray);
+  return ray.p_theta / (r * r);
 }
 
 float delta_phi(Ray ray) {
-  float b = constants_of_motion_b(ray);
-  float r = constants_radius(ray);
+  float b = ray.constants.b;
+  float r = radius(ray);
   float sin_theta = sin(ray.theta);
   return b / (r * r * sin_theta * sin_theta);
 }
 
 float delta_p_l(Ray ray) {
-  float B2 = constants_of_motion_B2(ray);
-  float r = constants_radius(ray);
+  float B2 = ray.constants.B2;
+  float r = radius(ray);
   float drdl = constants_drdl(ray);
   return B2 * B2 * constants_drdl(ray) / (r * r * r);
 }
 
 float delta_p_theta(Ray ray) {
-  float b = constants_of_motion_b(ray);
+  float b = ray.constants.b;
   float sin_theta = sin(ray.theta);
-  float r = constants_radius(ray);
+  float r = radius(ray);
   return b * b * cos(ray.theta) / (r * r  * sin_theta * sin_theta * sin_theta);
-}  
-
-Ray initial_ray(vec3 camera_dir) {
-  Ray ray;
-  vec3 cart_to_spherical = vec3(length(camera_dir), acos(camera_dir.z / length(camera_dir)), atan(camera_dir.y, camera_dir.x));
-  vec3 unit_vector_N = global_spherical_polar_basis(cart_to_spherical.yz);  
-//  vec3 unit_vector_N = global_spherical_polar_basis(camera_dir.yz);
-
-  ray.l = i_camera_pos.x;
-  ray.theta = i_camera_pos.y;
-  ray.phi = i_camera_pos.z;
-
-  unit_vector_N = normalize(unit_vector_N);
-  
-  ray.p_l = -unit_vector_N.x;
-  ray.p_theta = constants_radius(ray) * unit_vector_N.z;
-  ray.p_phi = constants_radius(ray) * sin(ray.theta) * -unit_vector_N.y;
-  
-  return ray;
-}
-
-float take_step(float curr_val, float delta) {
-  return curr_val - delta * STEP_SIZE;
-}
-
-void ray_take_step(inout Ray ray) {
-  /* Update camera position. */
-  Ray ray_cpy = ray;
-  ray.l = take_step(ray.l, delta_l(ray_cpy));
-  ray.theta = take_step(ray.theta, delta_theta(ray_cpy));
-  ray.phi = take_step(ray.phi, delta_phi(ray_cpy));
-  /* Update canonical momenta. */
-  ray.p_l = take_step(ray.p_l, delta_p_l(ray_cpy));
-  ray.p_theta = take_step(ray.p_theta, delta_p_theta(ray_cpy));
-}
-
-vec3 rand() {
-  vec3 random = vec3(fract(sin(dot(gl_FragCoord.xyz + vec3(0.0, 1.0, 2.0), vec3(12.9898, 78.233, 45.5432))) * 43758.5453),
-                   fract(sin(dot(gl_FragCoord.xyz + vec3(3.0, 4.0, 5.0), vec3(12.9898, 78.233, 45.5432))) * 43758.5453),
-                   fract(sin(dot(gl_FragCoord.xyz + vec3(6.0, 7.0, 8.0), vec3(12.9898, 78.233, 45.5432))) * 43758.5453));
-  return random;
 }
 
 /*
@@ -136,32 +105,73 @@ vec3 rand() {
   far from the wormhole so that the ray would not be bent before reaching the
   desired pixel (i.e., we assume the ray moves in a straight line from the camera
   to the pixel). */
-vec3 camera_direction() {
+vec3 cameraDirection() {
   vec2 normalized_coords = (gl_FragCoord.xy - i_resolution * 0.5) / min(i_resolution.x, i_resolution.y);
   const float tan_half_fov = tan(radians(FOV) * 0.5);
   vec3 direction = vec3(normalized_coords * tan_half_fov, 1.0);
   return normalize(direction);
 }
 
-void integrate_geodesic(inout Ray ray) {
-  for (float t = 0.0; t >= T_THRESHOLD; t -= STEP_SIZE) {
-    ray_take_step(ray);
-  }
+/* Initialize the ray, taking the direction in spherical coordinates. */
+Ray constructRay(vec2 dir) {
+  Ray ray;
+  vec3 unitVectorN = globalSphericalPolarBasis(dir);  
+
+  ray.l = i_camera_pos.x;
+  ray.theta = i_camera_pos.y;
+  ray.phi = i_camera_pos.z;
+
+  unitVectorN = normalize(unitVectorN);
+  
+  ray.p_l = -unitVectorN.x;
+  ray.p_theta = radius(ray) * unitVectorN.z;
+  ray.p_phi = radius(ray) * sin(ray.theta) * -unitVectorN.y;
+
+  ray.constants.b = constantsOfMotion_b(ray);
+  ray.constants.B2 = constantsOfMotion_B2(ray);
+
+  return ray;
+}
+
+float takeStep(float value, float delta) {
+  /* We subtract as t moves downwards. */
+  return value - delta * STEP_SIZE;
+}
+
+Ray rayTakeStep(in Ray ray) {
+  Ray next = ray;
+  /* Update position. */
+  next.l = takeStep(ray.l, delta_l(ray));
+  next.theta = takeStep(ray.theta, delta_theta(ray));
+  next.phi = takeStep(ray.phi, delta_phi(ray));
+  /* Update canonical momenta. */
+  next.p_l = takeStep(ray.p_l, delta_p_l(ray));
+  next.p_theta = takeStep(ray.p_theta, delta_p_theta(ray));
+  return next;
+}
+
+vec3 cartesianToSpherical(vec3 coord) {
+  float r = length(coord);
+  float theta = acos(coord.z / length(coord));
+  float phi = sign(coord.y) * acos(coord.x / sqrt(coord.x * coord.x + coord.y + coord.y));
+  return vec3(r, theta, phi);
 }  
 
-// as a simplification for now, let's just do one color if it hits the lower
-// celestial sphere and another color if it hits the other. 
+vec4 raytrace() {
+  Ray ray = constructRay(cartesianToSpherical(cameraDirection()).yz);
+  for (float t = 0.0; t >= T_THRESHOLD; t -= STEP_SIZE) {
+    ray = rayTakeStep(ray);
+  }
+  /* As a simplification for now, let's just do one color if it hits the lower
+  celestial sphere and another color if it hits the other. */
+  if (ray.l < 0.0) {
+    return vec4(1.0, 0.0, 0.0, 1.0);
+  } else {
+    return vec4(0.0, 0.0, 1.0, 1.0);
+  }    
+}
+
 
 void main() {
-  const vec4 red = vec4(1.0, 0.0, 0.0, 1.0);
-  const vec4 blue = vec4(0.0, 0.0, 1.0, 1.0);
-  Ray ray = initial_ray(camera_direction());
-  integrate_geodesic(ray);
-  if (ray.l < 0.0) {
-    color = red; 
-    // saturn side 
-  }
-  else {
-    color = blue;
-  }
+  color = raytrace();
 }
